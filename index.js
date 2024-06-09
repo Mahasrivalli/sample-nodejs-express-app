1efe245e0d95f6eac78b99d9cf94ac56df9c2beb
Mahasrivalli <22pa1a5762@vishnu.edu.in>
2024-06-09 15:25:10 +0530

    added express to application

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.loh("hi")
+const express = require('express')
+const app = express()
+const port = 3500
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
